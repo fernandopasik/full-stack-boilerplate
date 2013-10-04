e8c335911a06dca82275f4b9de57e5852e5816ba
Fernando Pasik <fernando@pasik.com.ar>
2013-10-04 07:23:21 -0300

    Server root route
    routes file included.
    root send ok status.
    tests.

--- server/server.js
+++ server/server.js
@@ -3,5 +3,6 @@
 var app = require('express')();
 
 require('./config')(app);
+require('./routes')(app);
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,12 +1,22 @@
 /* jshint expr: true, es3: false */
 'use strict';
 
-var app = require('../server/server');
+var app = require('../server/server'),
+    expect = require('chai').expect,
+    request = require('superagent');
 
 describe('Full Stack Application Test', function () {
 
     it('Server listens', function (done) {
         app.get('start')(done);
+    });
+    it('Access to root route', function (done) {
+        request
+            .get('http://localhost:9001/')
+            .end(function (res) {
+                expect(res.ok).to.be.true;
+                done();
+            });
     });
 
 });
